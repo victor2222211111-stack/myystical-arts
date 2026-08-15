5a315065507824ecfd249127fa1f505614b8007f
Oceanbeans Dev <victor2222211111@gmail.com>
2026-08-15 13:54:39 +0530

    Add Netlify configuration, serverless handler, and Supabase database schema

---
+++ netlify/functions/server.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const serverless = require('serverless-http');
+const app = require('../../server');
+
+module.exports.handler = serverless(app);
--- server.js
+++ server.js
@@ -154,7 +154,10 @@ async function start() {
   }
 }
 
-start();
+if (require.main === module) {
+  start();
+}
 
 module.exports = app;
+
 
